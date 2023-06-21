060d73c06ea936f632d78e68f95089a9192abf50
MahmoudEasa <mu01011422865@gmail.com>
2023-06-21 09:34:24 -0700

    Handle Free

---
+++ free_list.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * free_list - free linked list
+ * @head: the head of the linked list
+ */
+
+void free_list(stack_t **head)
+{
+	stack_t *temp, *help;
+
+	if (head && *head)
+	{
+		temp = (*head)->next;
+		while (temp != *head)
+		{
+			help = temp->next;
+			free(temp);
+			temp = help;
+		}
+		free(*head);
+		*head = NULL;
+	}
+}
+
