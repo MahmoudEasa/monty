b468c49366ba57603eb77480e12c39cacd61602e
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-22 15:51:08 -0700

    solve task rotl

--- _rotl.c
+++ _rotl.c
@@ -1,18 +1,31 @@
 #include "monty.h"
 
 /**
- * handle_rotl - ?????????
+ * handle_rotl - rotate stack
  * @head: a pointer to first node
  * @line_num: integer input
  */
 
 void handle_rotl(stack_t **head, unsigned int line_num)
 {
-	if (!head || !(*head))
-		handle_err("can't pchar,", "stack empty", line_num, head);
-	if ((*head)->n < 0 || (*head)->n > 127)
-		handle_err("can't pchar,", "value out of range", line_num, head);
+	stack_t *top, *last;
 
-	printf("rotl: %d\n", (*head)->n);
+	(void)line_num;
+	if (!(*head) || !(*head)->next)
+		return;
+	top = (*head)->next;
+	last = *head;
+
+	while (last->next)
+	{
+		last = last->next;
+		if (last == *head)
+			break;
+	}
+	top->prev = (*head)->prev;
+	last->next = *head;
+	(*head)->next = top;
+	(*head)->prev = last;
+	*head = top;
 }
 
